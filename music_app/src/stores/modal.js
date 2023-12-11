e56c143bdb3c46fa25d3c0ca2e2d8afc675f975a
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-12-11 16:04:07 +0700

    create store and disable rule

---
+++ music_app/src/stores/modal.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+const useModalStore = defineStore('modal', {
+  state: () => ({
+    isOpen: false
+  }),
+
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? 'hidden' : ''
+    }
+  }
+})
+
+export default useModalStore
