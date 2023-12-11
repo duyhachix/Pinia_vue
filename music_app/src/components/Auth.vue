6f52d90147b40602e8333a14410f84587f7dd666
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-12-11 16:48:40 +0700

    bind class to change tab

--- music_app/src/components/Auth.vue
+++ music_app/src/components/Auth.vue
@@ -29,18 +29,32 @@
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
               <a
-                class="block rounded py-3 px-4 transition hover:text-white text-white bg-blue-600"
+                class="block rounded py-3 px-4 transition"
                 href="#"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register'
+                }"
+                @click.prevent="tab = 'login'"
                 >Login</a
               >
-            </li>
+            </li> 
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" href="#">Register</a>
+              <a
+                class="block rounded py-3 px-4 transition"
+                href="#"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login'
+                }"
+                @click.prevent="tab = 'register'"
+                >Register</a
+              >
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -67,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -147,14 +161,22 @@ import useModalStore from '@/stores/modal'
 export default {
   name: 'Auth',
 
+  data() {
+    return {
+      tab: 'login'
+    }
+  },
+
   computed: {
     ...mapState(useModalStore, ['hiddenClass']),
-    ...mapWritableState(useModalStore, ['isOpen'])
+    ...mapWritableState(useModalStore, {
+      modalVisibility: 'isOpen' // use alias for 'isOpen' state, just this component use 'modalVisibility'
+    })
   },
 
   methods: {
     onCloseModal() {
-      this.isOpen = false
+      this.modalVisibility = false
     }
   }
 }
