0985c2e062e26e1c252250a4d06736312394b931
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-12-19 21:50:50 +0700

    :lollipop: setting_up_login

--- music_app/src/components/LoginForm.vue
+++ music_app/src/components/LoginForm.vue
@@ -40,6 +40,9 @@
 </template>
 
 <script>
+import { mapActions } from 'pinia';
+import useUserStore from '@/stores/user';
+
 export default {
   name: 'LoginForm',
 
@@ -57,12 +60,22 @@ export default {
   },
 
   methods: {
-    onLogin(values) {
-      console.log('login successful', values);
+    ...mapActions(useUserStore, ['authenticate']),
+
+    async onLogin(values) {
       this.login_in_submission = true;
       this.login_show_alert = true;
-      this.login_alert_variant = 'bg-red-400';
-      this.login_alert_message = 'Successfully login.';
+      this.login_alert_variant = 'bg-blue-400';
+      this.login_alert_message = 'Please wait...';
+
+      try {
+        await this.authenticate(values);
+      } catch (e) {
+        this.login_in_submission = false;
+        this.login_alert_variant = 'bg-red-400';
+        this.login_alert_message = e.message;
+        return;
+      }
 
       this.login_alert_variant = 'bg-green-500';
       this.login_alert_message = 'Successfully you are now logged in.';
