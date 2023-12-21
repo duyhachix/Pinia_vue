45eae7032a5b23f7eec70c9a5202b6cf8146e185
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-12-21 23:43:39 +0700

    :bar_chart: handling progress bar when upload

--- music_app/src/components/UploadSection.vue
+++ music_app/src/components/UploadSection.vue
@@ -21,32 +21,15 @@
       </div>
       <hr class="my-6" />
       <!-- Progess Bars -->
-      <div class="mb-4">
+      <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
-        <div class="font-bold text-sm">Just another song.mp3</div>
+        <div class="font-bold text-sm">{{ upload.name }}</div>
         <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
           <!-- Inner Progress Bar -->
           <div
             class="transition-all progress-bar bg-blue-400"
-            style="width: 75%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 35%"
-          ></div>
-        </div>
-      </div>
-      <div class="mb-4">
-        <div class="font-bold text-sm">Just another song.mp3</div>
-        <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-          <div
-            class="transition-all progress-bar bg-blue-400"
-            style="width: 55%"
+            :class="'bg-blue-400'"
+            :style="{ width: upload.current_progress + '%' }"
           ></div>
         </div>
       </div>
@@ -61,6 +44,7 @@ export default {
   data() {
     return {
       is_dragover: false,
+      uploads: [],
     };
   },
   methods: {
@@ -79,8 +63,24 @@ export default {
         // storageBucket : music-pinia.appspot.com,
         const storageRef = storage.ref();
         const songsRef = storageRef.child(`songs/${file.name}`); // 'music-pinia.appspot.com/songs/example.mp3'',
+
         // upload file to firebase storage
-        songsRef.put(file);
+        let task = songsRef.put(file);
+
+        let uploadIndex =
+          this.uploads.push({
+            task,
+            current_progress: 0,
+            name: file.name,
+          }) - 1;
+
+        task.on('state_changed', (snapshot) => {
+          let percentage =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          // re-assign precentage value
+          this.uploads[uploadIndex].current_progress = percentage;
+          console.log(`Upload is ${percentage}%/100% done`);
+        });
       });
 
       console.log(files);
