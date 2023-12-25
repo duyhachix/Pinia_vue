5dd5beef73ab1f0c504e47164d190b8e2297c3c2
Thanhduy_Hachi-x <duy.nguyen@hachi-x.com>
2023-12-25 16:12:47 +0700

    :musical_keyboard: toggle play/pause music with Howl

--- music_app/src/stores/player.js
+++ music_app/src/stores/player.js
@@ -1,14 +1,38 @@
 import { defineStore } from 'pinia';
+import { Howl } from 'howler';
 
 const usePlayerStore = defineStore('player', {
   state: () => ({
     currentSong: {},
+    sound: {},
   }),
+
+  getters: {
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
+  },
 
   actions: {
     async newSong(song) {
       console.log('newSong action', song);
       this.currentSong = song;
+      // use Howl to create a song object
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true,
+      });
+
+      this.sound.play();
+    },
+
+    async toggleAudio() {
+      if (!this.sound.playing) return;
+
+      this.sound.playing() ? this.sound.pause() : this.sound.play();
     },
   },
 });
